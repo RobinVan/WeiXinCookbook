b5a7c0b9538658cccc5eec41ae0eab8de681bc3e
lufo <lufo816@gmail.com>
2014-11-30 22:52:02 +0800

    use database

--- getCaipu.py
+++ getCaipu.py
@@ -4,19 +4,28 @@ import urllib2, json, chardet, os
 def getCaipu(id):
     APPKEY = '772138d3588d780c86591a4eedf0f634'
     url = 'http://apis.juhe.cn/cook/queryid?key=' + APPKEY + '&id='
+    db = MySQLdb.connect(host='localhost',user='root',passwd='lxb',db='caipu')
+    cursor = db.cursor()
+    count = cursor.execute("""select * from caipu where id = %s""",(id,))
     caipu_information = {}
-    if not(os.path.isfile('json/' + 'caipu' + str(id) + '.json')) :
+    #if not(os.path.isfile('json/' + 'caipu' + str(id) + '.json')) :
+    if count == 0 :
         try:
             res = urllib2.urlopen(url + str(id))
             res_read_uni = res.read().decode("utf-8-sig")
             #print chardet.detect(res_read_uni)
-            jsonFile = open('json/' + 'caipu' + str(id) + '.json', 'w')
-            jsonFile.write(unicode.encode(res_read_uni, 'utf-8'))
+            #jsonFile = open('json/' + 'caipu' + str(id) + '.json', 'w')
+            #jsonFile.write(unicode.encode(res_read_uni, 'utf-8'))
             data = json.loads(res_read_uni)
             reason = data['reason']
             if reason == 'Success':
                 result = data['result']['data']
                 caipu = result[0]
+                url = 'http://104.131.156.81/' + 'caipu' + str(id)
+                cursor.execute("""insert into caipu values(%s,%s,%s,%s,%s)""",(id, caipu['title'], caipu['imtro'], caipu['albums'][0], url,))
+                db.commit()
+                cursor.close()
+                db.close()
                 f = open('templates/' + 'caipu' + str(id) + '.html', 'w')
                 f.write(unicode.encode(u'<meta http-equiv="Content-Type" content="width=device-width, initial-scale=1, charset=utf-8"/>\n', 'utf-8'))
                 f.write(unicode.encode('<center><h2>' + caipu['title'] + '</h2></center>\n', 'utf-8'))
@@ -39,23 +48,24 @@ def getCaipu(id):
                 f.close()
                 caipu_information['title'] = caipu['title']
                 caipu_information['description'] = caipu['imtro']
-                caipu_information['picurl'] = 'null'
+                #caipu_information['picurl'] = 'null'
                 caipu_information['picurl'] = caipu['albums'][0]
-                caipu_information['url'] = 'http://104.131.156.81/' + 'caipu' + str(id)
+                caipu_information['url'] = url
                 print 'get caipu Success'
             else:
                 print 'get caipu ERROR'
         except urllib2.URLError,e:
             print e.reason
     else:
-        jsonFile = file('json/' + 'caipu' + str(id) + '.json')
-        data = json.load(jsonFile)
-        result = data['result']['data']
-        caipu = result[0]
-        caipu_information['title'] = caipu['title']
-        caipu_information['description'] = caipu['imtro']
-        caipu_information['picurl'] = 'null'
-        caipu_information['picurl'] = caipu['albums'][0]
-        caipu_information['url'] = 'http://104.131.156.81/' + 'caipu' + str(id)
+        #jsonFile = file('json/' + 'caipu' + str(id) + '.json')
+        #data = json.load(jsonFile)
+        #result = data['result']['data']
+        #caipu = result[0]
+        caipu = cursor.fetchall()
+        caipu_information['title'] = caipu[1]
+        caipu_information['description'] = caipu[2]
+        #caipu_information['picurl'] = 'null'
+        caipu_information['picurl'] = caipu[3]
+        caipu_information['url'] = caipu[4]
 
     return caipu_information
