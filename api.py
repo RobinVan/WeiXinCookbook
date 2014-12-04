fe972cc52fb10b75a53b1bd15a7af70d90777793
lufo <lufo816@gmail.com>
2014-12-04 20:49:27 +0800

    api

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ class api:
         if content == 'today':
             caipu_list = range(1, 80452)
             return getList(caipu_list)
-        else
+        else:
             if content in tags.tags.keys():
                 #response = wechat.response_text(tags.tags[content])
                 id_list = getIDFromTag.getIDFromTag(tags.tags[content])
@@ -25,19 +25,19 @@ class api:
             else:
                 return '不能识别'
 
-class getList(id_list):
-    news_list = []
-    for id in id_list:
-        caipu = getCaipu.getCaipu(id)
-        news_map = {
-            'title': caipu['title'],
-            'description': caipu['description'],
-            'picurl': caipu['picurl'],
-            #'url':'http://104.131.156.81/test'
-            'url': caipu['url']
-        }
-        news_list.append(news_map)
-    return news_list
+    def getList(id_list):
+        news_list = []
+        for id in id_list:
+            caipu = getCaipu.getCaipu(id)
+            news_map = {
+                'title': caipu['title'],
+                'description': caipu['description'],
+                'picurl': caipu['picurl'],
+                #'url':'http://104.131.156.81/test'
+                'url': caipu['url']
+            }
+            news_list.append(news_map)
+        return news_list
 
 
 if __name__ == '__main__':
