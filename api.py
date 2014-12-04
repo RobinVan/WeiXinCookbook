25b5bcab4c115736d3b13a8f1deaacb117c19476
lufo <lufo816@gmail.com>
2014-12-04 20:46:10 +0800

    api

--- api.py
+++ api.py
@@ -6,30 +6,39 @@ urls = (
     '/', 'api'
 )
 app = web.application(urls, globals())
+
 class api:
     def GET(self):
         content = web.input()['caipu']
         #print content
-        if content in tags.tags.keys():
-            #response = wechat.response_text(tags.tags[content])
-            id_list = getIDFromTag.getIDFromTag(tags.tags[content])
-        else:
-            id_list = getIDFromName.getIDFromName(content)
-        if id_list:
-            news_list = []
-            for id in id_list:
-                caipu = getCaipu.getCaipu(id)
-                news_map = {
-                    'title': caipu['title'],
-                    'description': caipu['description'],
-                    'picurl': caipu['picurl'],
-                    #'url':'http://104.131.156.81/test'
-                    'url': caipu['url']
-                }
-                news_list.append(news_map)
-            return news_list
-        else:
-            return '不能识别'
+        if content == 'today':
+            caipu_list = range(1, 80452)
+            return getList(caipu_list)
+        else
+            if content in tags.tags.keys():
+                #response = wechat.response_text(tags.tags[content])
+                id_list = getIDFromTag.getIDFromTag(tags.tags[content])
+            else:
+                id_list = getIDFromName.getIDFromName(content)
+            if id_list:
+                return getList(id_list)
+            else:
+                return '不能识别'
+
+class getList(id_list):
+    news_list = []
+    for id in id_list:
+        caipu = getCaipu.getCaipu(id)
+        news_map = {
+            'title': caipu['title'],
+            'description': caipu['description'],
+            'picurl': caipu['picurl'],
+            #'url':'http://104.131.156.81/test'
+            'url': caipu['url']
+        }
+        news_list.append(news_map)
+    return news_list
+
 
 if __name__ == '__main__':
     app.run()
